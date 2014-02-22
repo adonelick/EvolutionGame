7336b308f9a78f87cfd931a8f465aafc2304b9ad
Andrew Donelick <cs121@Js-MacBook-Pro-3.local>
2014-02-22 11:41:26 -0800

    Added configuration file

---
+++ EvolutionaryGame/EvolutionaryGame/config.h
@@ -0,0 +1,20 @@
+//
+//  config.h
+//  EvolutionaryGame
+//
+//  Created by CS121 on 2/22/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+/*
+ Defines the constants needed to make the program run 
+ smoothly and effectively. 
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
+#define MAX_VELOCITY 10.0
+#define MIN_VELOCITY 0.0
+
+#endif
