a5c7c355589098d21297430ec11252fb73c2e3be
Andrew Donelick <cs121@Js-MacBook-Pro-3.local>
2014-02-22 11:24:11 -0800

    Added the objects folder, along with the Platform class.

---
+++ EvolutionaryGame/EvolutionaryGame/Objects/Platform.h
@@ -0,0 +1,13 @@
+//
+//  Platform.h
+//  EvolutionaryGame
+//
+//  Created by CS121 on 2/22/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Platform : SKSpriteNode
+
+@end
---
+++ EvolutionaryGame/EvolutionaryGame/Platform.m
@@ -0,0 +1,13 @@
+//
+//  Platform.m
+//  EvolutionaryGame
+//
+//  Created by CS121 on 2/22/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import "Platform.h"
+
+@implementation Platform
+
+@end
